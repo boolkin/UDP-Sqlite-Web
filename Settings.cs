926b59e996d80ac26fc099acf4b28f21b5992528
boolkin <weronkz@gmail.com>
2026-05-23 14:51:22 +0300

    udp

---
+++ Settings.cs
@@ -0,0 +1,24 @@
+public class UdpSettings
+{
+    public int Port { get; set; }
+    public string DataType { get; set; } = "float";
+    public string ByteOrder { get; set; } = "ABCD";
+    public bool EnableInfoLogging { get; set; }
+}
+
+public class DatabaseSettings
+{
+    public string FileName { get; set; } = "data2.db";
+    public List<string> Tables { get; set; } = new();
+    public int ColumnCount { get; set; } = 10;
+    public List<string> Headers { get; set; } = new();
+    public int RetentionDays { get; set; } = 3;
+}
+
+public class TriggerSettings
+{
+    public bool Enabled { get; set; } = true;
+    public int IntervalHours { get; set; } = 12;
+    public double Threshold { get; set; } = 0.9;
+    public int TimezoneShift { get; set; } = -5;
+}
